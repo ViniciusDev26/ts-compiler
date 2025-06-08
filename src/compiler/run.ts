68cff2254d268ab223653f4332bcf251f6623575
Carlos Vinicius <viniciusdev.26@gmail.com>
2025-06-08 00:25:43 -0300

    📝 update project roadmap to include team members and remove completed tasks; clean up console log in run.ts

--- src/compiler/run.ts
+++ src/compiler/run.ts
@@ -8,7 +8,6 @@ async function bootstrap() {
   const input = await readFile("src/input-source/index.kat", "utf-8");
 
   const tokens = lexer(input);
-  console.log(tokens);
   const ast = parser(tokens);
   const JS = generateJsCode(ast);
 
